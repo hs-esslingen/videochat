8b1f65f983d63f5ac0abc584844ff5e452158aa7
Leon Schmidt <lescit01@hs-esslingen.de>
2020-04-02 17:02:45 +0200

    add websocket, notify if somebody joins or leaves

--- src-server/server.ts
+++ src-server/server.ts
@@ -1,23 +1,58 @@
 import * as express from "express";
-import { api } from './api';
-import {join} from "path";
+import { Api } from "./api";
+import { join } from "path";
+import * as WebSocket from "ws";
+import * as http from "http";
 
 // Express server
 const app = express();
+const server = http.createServer(app);
 
 const PORT = process.env.PORT || 4000;
 
-console.log(__dirname);
+const wss = new WebSocket.Server({ server: server, path: "/ws" }, () => {
+  console.log("websocket opened");
+});
 
-app.use("/api", api);
+wss.on("error", err => {
+  console.error(err);
+});
 
-app.use("/", express.static(join(__dirname,"./browser")))
+
+const api = new Api(wss);
+
+
+app.use("/api", api.getApi());
+
+app.use("/", express.static(join(__dirname, "./browser")));
 app.use("*", (req, res) => {
-  console.log(join(__dirname, "./browser/index.html"));
-  res.sendFile(join(__dirname, "./browser/index.html"))
-})
+  console.log(req.path);
+  res.sendFile(join(__dirname, "./browser/index.html"));
+});
+
+wss.on("connection", function connection(ws: MyWebSocket) {
+  ws.isAlive = true;
+  ws.on("pong", () => ws.isAlive = true);
+});
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws: MyWebSocket) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping(() => {});
+  });
+}, 10000);
+
+wss.on("close", function close() {
+  clearInterval(interval);
+});
 
 // Start up the Node server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
+
+export interface MyWebSocket extends WebSocket {
+  isAlive: boolean;
+}
