3b0236fc91295a6b5ea2687483629705041e26a5
Leon Schmidt <le.sch@live.de>
2020-04-30 12:57:35 +0200

    fix node_env variable

--- src/app/helper/local-media.service.ts
+++ src/app/helper/local-media.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable({
   providedIn: "root",
 })
-export class LocalMediaService {  
+export class LocalMediaService {
   private autoGainControl: boolean;
   private video: MediaStream;
   private videoLabel: string;
