6849f00e8c009fbfe89242b53ed60e3caeef6cb9
Leon Schmidt <le.sch@live.de>
2020-10-28 17:53:25 -0400

    fix blackbaord auth

--- src-server/blackboard-oauth.ts
+++ src-server/blackboard-oauth.ts
@@ -3,29 +3,44 @@ import * as passport from 'passport';
 import * as express from 'express';
 import * as OAuth2Strategy from 'passport-oauth2';
 import {VerifyCallback} from 'passport-oauth2';
+import fetch from 'node-fetch';
 
 export const logger = getLogger('blackboard');
 
 export function setupBlackboard(app: express.Application) {
   const oauthStrategy = new OAuth2Strategy(
     {
-      authorizationURL: 'https://gannon.blackboard.com/learn/api/public/v1/oauth2/authorizationcode',
-      tokenURL: 'https://gannon.blackboard.com/learn/api/public/v1/oauth2/token',
+      authorizationURL: process.env.OAUTH_URL + '/learn/api/public/v1/oauth2/authorizationcode',
+      tokenURL: process.env.OAUTH_URL + '/learn/api/public/v1/oauth2/token',
       clientID: process.env.OAUTH_CLIENT_ID as string,
       clientSecret: process.env.OAUTH_CLIENT_SECRET as string,
+      customHeaders: {
+        Authorization: 'Basic ' + Buffer.from(process.env.OAUTH_CLIENT_ID + ':' + process.env.OAUTH_CLIENT_SECRET).toString('base64'),
+      },
+
+      pkce: false,
       callbackURL: process.env.CALLBACK_URL,
       scope: 'read',
     },
-    (accessToken: string, refreshToken: string, profile: OAuth2Strategy.Metadata, done: VerifyCallback) => {
+    async (accessToken: string, refreshToken: string, profile: OAuth2Strategy.Metadata, done: VerifyCallback) => {
       logger.debug('Parsing', profile);
       const user = {
         accessToken,
         refreshToken,
         displayName: 'User',
-        email: 'user@test.de',
+        email: 'unknown@test.de',
       };
+      console.log(accessToken);
+      console.log(profile);
+      const request = await fetch(process.env.OAUTH_URL + '/learn/api/public/v1/users', {
+        headers: {Authorization: 'Bearer ' + accessToken},
+      });
 
-      //TODO: Get User Email and DisplayName
+      const data = await request.json();
+      if (data.results?.length >= 1) {
+        user.displayName = data.results[0].name.given + ' ' + data.results[0].name.family;
+        user.email = data.results[0].userName + '@gannon.edu';
+      }
       return done(null, user);
     }
   );
@@ -51,8 +66,7 @@ export function setupBlackboard(app: express.Application) {
           </html>`);
   });
 
-  // Backwards compatibility to old metadata configuration
-  app.post('/login/callback', passport.authenticate('saml', {failureRedirect: '/auth/fail'}), (req, res) => {
+  app.get('/auth/callback', passport.authenticate('oauth2', {failureRedirect: '/auth/fail'}), (req, res) => {
     logger.info('SSO Login', req.user);
 
     res.send(`<html>
