a8e717721cd15027d2b0ec0503704420fe499973
test <test@test.com>
2020-11-26 19:57:47 -0500

    fix linting errors

--- src/app/poll/poll.component.spec.ts
+++ src/app/poll/poll.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PollComponent } from './poll.component';
+import {PollComponent} from './poll.component';
 
 describe('PollComponent', () => {
   let component: PollComponent;
@@ -8,9 +8,8 @@ describe('PollComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PollComponent ]
-    })
-    .compileComponents();
+      declarations: [PollComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
